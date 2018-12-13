967580143e4d4aa67b7cf9df9c59621d700ca6a6
theForerunner <liet2026@163.com>
2018-12-13 19:11:12 +0800

    Change VolleyRequest.getJSONObject() into a more general way

--- app/src/main/java/com/example/l/EtherLet/model/PostList.java
+++ app/src/main/java/com/example/l/EtherLet/model/PostList.java
@@ -2,6 +2,8 @@ package com.example.l.EtherLet.model;
 
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.l.EtherLet.R;
 import com.example.l.EtherLet.network.VolleyCallback;
 import com.example.l.EtherLet.network.VolleyRequest;
@@ -14,7 +16,7 @@ public class PostList implements PostListInterface {
 
     @Override
     public void getPostListData(final LoadDataCallBack callBack, Context context) {
-        VolleyRequest.getJSONObject(context.getString(R.string.host_url_real_share) + context.getString(R.string.get_theme_list_path), null, context, new VolleyCallback() {
+        VolleyRequest.getJSONObject(JsonObjectRequest.Method.POST, context.getString(R.string.host_url_real_share) + context.getString(R.string.get_theme_list_path), null, context, new VolleyCallback() {
             @Override
             public void onSuccess(JSONObject jsonObject) {
                 callBack.onSuccess(jsonObject);
@@ -29,7 +31,7 @@ public class PostList implements PostListInterface {
 
     @Override
     public void addNewPost(final LoadDataCallBack callBack, Context context, Map<String, Object> map) {
-        VolleyRequest.getJSONObject(context.getString(R.string.host_url_real) + context.getString(R.string.add_theme_path), map, context, new VolleyCallback() {
+        VolleyRequest.getJSONObject(JsonObjectRequest.Method.POST, context.getString(R.string.host_url_real) + context.getString(R.string.add_theme_path), map, context, new VolleyCallback() {
             @Override
             public void onSuccess(JSONObject jsonObject) {
                 callBack.onSuccess(jsonObject);
