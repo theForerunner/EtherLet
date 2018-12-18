42101018ac71b6b791aea86424b3d63c72d9730d
theForerunner <liet2026@163.com>
2018-12-18 21:24:41 +0800

    Temp commit

--- app/src/main/java/com/example/l/EtherLet/model/UserModel.java
+++ app/src/main/java/com/example/l/EtherLet/model/UserModel.java
@@ -1,23 +1,10 @@
 package com.example.l.EtherLet.model;
 
 public class UserModel {
-    private String userId;
-    private String password;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private int userId;
+    private String userAccount;
+    private String userPassword;
+    private String userUsername;
+    private String userKey;
 
 }
